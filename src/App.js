4c04a536fb6babda094a3d4996698d101dd640e0
503134279 <Swapnesh.Sangle1@ge.com>
2020-03-05 12:48:25 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!!</h1>
+                <h2>Welcome to your First React App...!</h2>
+                <p>Yay webpack done!!!</p>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+//webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './dist/index.html'
+        })
+    ]
+}
